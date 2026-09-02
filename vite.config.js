a947811869c091c582d328dbedf60af4d34a0554
effortw21 <tinna21abeba@gmail.com>
2026-09-02 05:01:38 +0300

    some editions

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/My-React_Portofolio/",
+  base: "/",
+  // optional: set a default dev server host (or remove this line)
+  server: { host: true },
 });
